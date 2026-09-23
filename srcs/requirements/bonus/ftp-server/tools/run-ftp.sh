a2cb98bbee5193f2c78798ce9e3203cd3db03427
efoyer <emilienfoyer@gmail.com>
2026-09-23 15:26:27 +0200

    Ajout site web static (attention les yeux)

--- srcs/requirements/bonus/ftp-server/tools/run-ftp.sh
+++ srcs/requirements/bonus/ftp-server/tools/run-ftp.sh
@@ -3,6 +3,7 @@
 adduser $FTP_USR --disabled-password --gecos ""
 echo "$FTP_USR:$FTP_PWD" | chpasswd
 chown -R $FTP_USR:$FTP_USR /var/www/html
+chown -R $FTP_USR:$FTP_USR /var/www/wordpress
 
 echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
 
