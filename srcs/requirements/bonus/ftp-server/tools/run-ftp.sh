3c45d974f649dd57a61af9321bc8acb74c1bf664
efoyer <emilienfoyer@gmail.com>
2026-09-23 11:38:11 +0200

    Add FTP-server

---
+++ srcs/requirements/bonus/ftp-server/tools/run-ftp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+adduser $FTP_USR --disabled-password --gecos ""
+echo "$FTP_USR:$FTP_PWD" | chpasswd
+chown -R $FTP_USR:$FTP_USR /var/www/html
+
+echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+
+exec vsftpd /etc/vsftpd.conf
